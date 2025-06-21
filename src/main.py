4f47187d7086845f94a659da717c9bb4a8708fa7
niraj-khatiwada <niraj@endtherig.com>
2025-06-21 11:22:19 +0545

    init

---
+++ src/main.py
@@ -0,0 +1,15 @@
+name: str = "Niraj"
+
+number: int = 100
+
+decimal: float = 100
+
+
+def greet(name: str) -> str:
+    return f"Hello {name}"
+
+
+print(greet("Niraj"))
+
+
+arrays: list[str] = ["Niraj"]
